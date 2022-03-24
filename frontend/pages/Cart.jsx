9c07ac8a5a17208b5f3b0154fb724c07fda6ff87
osefatul <sefaullahomar2@gmail.com>
2022-03-23 23:35:06 -0700

    working on cart page

--- frontend/pages/Cart.jsx
+++ frontend/pages/Cart.jsx
@@ -2,163 +2,80 @@ import React from "react";
 
 function Cart() {
   return (
-    <div className=" h-almost flex p-14">
+    <div className=" h-almost w-screen flex flex-col md:flex-row justify-around p-14">
       {/* LEFT SIDE OF THE PAGE */}
+      <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-center">
+                <thead className="border-b bg-gray-50">
+                  <tr className="">
+                    <th className="px-8 py-2 text-sm text-gray-500">Product</th>
+                    <th className="px-8 py-2 text-sm text-gray-500">Name</th>
+                    <th className="px-8 py-2 text-sm text-gray-500">Extras</th>
+                    <th className="px-8 py-2 text-sm text-gray-500">Price</th>
+                    <th className="px-8 py-2 text-sm text-gray-500">
+                      Quantity
+                    </th>
+                    <th className="px-8 py-2 text-sm text-gray-500">Total</th>
+                  </tr>
+                </thead>
 
-      <table>
-        <thead class="bg-gray-50">
-          <tr>
-            <th class="px-6 py-2 text-xs text-gray-500">ID</th>
-            <th class="px-6 py-2 text-xs text-gray-500">Name</th>
-            <th class="px-6 py-2 text-xs text-gray-500">Email</th>
-            <th class="px-6 py-2 text-xs text-gray-500">Created_at</th>
-            <th class="px-6 py-2 text-xs text-gray-500">Edit</th>
-            <th class="px-6 py-2 text-xs text-gray-500">Delete</th>
-          </tr>
-        </thead>
-        <tbody class="bg-white">
-          <tr class="whitespace-nowrap">
-            <td class="px-6 py-4 text-sm text-gray-500">1</td>
-            <td class="px-6 py-4">
-              <div class="text-sm text-gray-900">Jon doe</div>
-            </td>
-            <td class="px-6 py-4">
-              <div class="text-sm text-gray-500">jhondoe@example.com</div>
-            </td>
-            <td class="px-6 py-4 text-sm text-gray-500">2021-1-12</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="px-4 py-1 text-sm text-white bg-blue-400 rounded"
-              >
-                Edit
-              </a>
-            </td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="px-4 py-1 text-sm text-white bg-red-400 rounded"
-              >
-                Delete
-              </a>
-            </td>
-          </tr>
-          <tr class="whitespace-nowrap">
-            <td class="px-6 py-4 text-sm text-gray-500">1</td>
-            <td class="px-6 py-4">
-              <div class="text-sm text-gray-900">Jon doe</div>
-            </td>
-            <td class="px-6 py-4">
-              <div class="text-sm text-gray-500">jhondoe@example.com</div>
-            </td>
-            <td class="px-6 py-4 text-sm text-gray-500">2021-1-12</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="px-4 py-1 text-sm text-white bg-blue-400 rounded"
-              >
-                Edit
-              </a>
-            </td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="px-4 py-1 text-sm text-white bg-red-400 rounded"
-              >
-                Delete
-              </a>
-            </td>
-          </tr>
-          <tr class="whitespace-nowrap">
-            <td class="px-6 py-4 text-sm text-gray-500">1</td>
-            <td class="px-6 py-4">
-              <div class="text-sm text-gray-900">Jon doe</div>
-            </td>
-            <td class="px-6 py-4">
-              <div class="text-sm text-gray-500">jhondoe@example.com</div>
-            </td>
-            <td class="px-6 py-4 text-sm text-gray-500">2021-1-12</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="px-4 py-1 text-sm text-white bg-blue-400 rounded"
-              >
-                Edit
-              </a>
-            </td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="px-4 py-1 text-sm text-white bg-red-400 rounded"
-              >
-                Delete
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      {/* <div className="">
-        <table className="table-auto">
-          <thead>
-            <tr className="">
-              <th>Product</th>
-              <th>Name</th>
-              <th>Extras</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-            </tr>
-          </thead>
+                <tbody className="bg-white">
+                  <tr className="whitespace-nowrap">
+                    <td className="px-8 py-2 text-sm text-gray-500">
+                      <div className="w-[5rem] h-[5rem]">
+                        <img className="w-full" src="/img/pizza.png" alt="" />
+                      </div>
+                    </td>
 
-          <tbody>
-            <tr>
-              <td>
-                <div className="w-[5rem] h-[5rem]">
-                  <img className="w-full" src="/img/pizza.png" alt="" />
-                </div>
-              </td>
-              <td>
-                <span>Mike</span>
-              </td>
-              <td>
-                <span>Double Infrediants, Spicy Sauce</span>
-              </td>
-              <td>
-                <span>$20.21</span>
-              </td>
-              <td>
-                <span>3</span>
-              </td>
-              <td>
-                <span>60.63</span>
-              </td>
-            </tr>
+                    <td className="px-8 py-2 text-sm">
+                      <span>Mike</span>
+                    </td>
 
-            <tr>
-              <td>
-                <div className="w-[5rem] h-[5rem]">
-                  <img className="w-full" src="/img/pizza.png" alt="" />
-                </div>
-              </td>
-              <td>
-                <span>Henry</span>
-              </td>
-              <td>
-                <span>Double Infrediants, Spicy Sauce</span>
-              </td>
-              <td>
-                <span>$20.21</span>
-              </td>
-              <td>
-                <span>3</span>
-              </td>
-              <td>
-                <span>60.63</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div> */}
+                    <td className="px-10 py-2 text-sm">
+                      <span>Double Infrediants, Spicy Sauce</span>
+                    </td>
+                    <td className="px-8 py-2 text-sm">
+                      <span>$20.21</span>
+                    </td>
+                    <td className="px-8 py-2 text-sm justify-items-center ">
+                      <span className="">3</span>
+                    </td>
+                    <td className="px-8 py-2 text-sm">
+                      <span>60.63</span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <div className="w-[5rem] h-[5rem]">
+                        <img className="w-full" src="/img/pizza.png" alt="" />
+                      </div>
+                    </td>
+                    <td>
+                      <span>Henry</span>
+                    </td>
+                    <td>
+                      <span>Double Infrediants, Spicy Sauce</span>
+                    </td>
+                    <td>
+                      <span>$20.21</span>
+                    </td>
+                    <td>
+                      <span>3</span>
+                    </td>
+                    <td>
+                      <span>60.63</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* RIGHT SIDE OF THE PAGE */}
       <div>
