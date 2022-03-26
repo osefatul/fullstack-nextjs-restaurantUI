49b82b682e1b2abbd3263bd52c64ba915e08152f
osefatul <sefaullahomar2@gmail.com>
2022-03-25 17:08:22 -0700

    trying to remove env file

--- frontend/models/Product.js
+++ frontend/models/Product.js
@@ -1,8 +1,43 @@
 import mongoose from "mongoose";
 
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxLength: 60,
+    },
 
-const ProductSchema = new mongoose.Schema({
-  title: {
-    type:
-  }
-})
+    desc: {
+      type: String,
+      required: true,
+      maxLength: 200,
+    },
+
+    img: {
+      type: String,
+      required: true,
+    },
+
+    prices: {
+      type: [Number],
+      required: true,
+    },
+
+    // This is when we choose those different options for pizza it will bring changes to the prices as well.
+    extraOptions: {
+      type: [
+        {
+          text: { type: String, required: true },
+          price: { type: Number, required: true },
+        },
+      ],
+    },
+  },
+
+  { timestamps: true }
+);
+
+// This look in to the database if there is already a model for product created. yes, then don't create another one, if no, then create a new one.
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
