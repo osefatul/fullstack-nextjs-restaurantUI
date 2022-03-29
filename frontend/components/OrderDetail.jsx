de476488eee6257ca084cc1f77bb4c871f7ac998
osefatul <sefaullahomar2@gmail.com>
2022-03-29 03:47:12 -0700

    Cash on delivery component

--- frontend/components/OrderDetail.jsx
+++ frontend/components/OrderDetail.jsx
@@ -9,33 +9,42 @@ const OrderDetail = ({ total, createOrder }) => {
   };
 
   return (
-    <div className="absolute w-[100%] bg-[#776f6f70] h-screen flex top-0 left-0 right-0 justify-center items-center">
+    <div className="absolute w-[100%] bg-[#776f6f70] h-almost flex top-0 left-0 right-0 justify-center items-center">
       <div className="bg-white w-[40rem] rounded p-16 flex flex-col justify-center items-center z-50 ">
-        <h1 className="text-3xl font-bold">You will pay $12 after delivery.</h1>
-        <div className="">
+        <h1 className="text-3xl font-bold pb-8">
+          You will pay $12 after delivery.
+        </h1>
+        <div className="flex flex-col w-full pb-2">
           <label className="">Name Surname</label>
           <input
             placeholder="John Doe"
             type="text"
-            className="border"
+            className="border pl-2"
             onChange={(e) => setCustomer(e.target.value)}
           />
         </div>
-        <div className="">
+        <div className=" flex flex-col  w-full pb-2">
           <label className="">Phone Number</label>
-          <input type="text" placeholder="+1 234 567 89" className="border" />
+          <input
+            type="text"
+            placeholder="+1 234 567 89"
+            className="border pl-2"
+          />
         </div>
-        <div className="">
+        <div className="flex flex-col w-full mb-3 ">
           <label className="">Address</label>
           <textarea
             rows={5}
             placeholder="Elton St. 505 NY"
             type="text"
-            className="border"
+            className="border pl-2"
             onChange={(e) => setAddress(e.target.value)}
           />
         </div>
-        <button className="" onClick={handleClick}>
+        <button
+          className="border w-20 rounded bg-[teal] text-white cursor-pointer"
+          onClick={handleClick}
+        >
           Order
         </button>
       </div>
