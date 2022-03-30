f5b805b49a127d78a8af56c309598b7e10c05822
osefatul <sefaullahomar2@gmail.com>
2022-03-29 23:56:08 -0700

    udpating admin page

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,6 +1,11 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function index() {
+  const [pizzaList, setPizzaList] = useState(products);
+  const [orderList, setOrderList] = useState(orders);
+  const status = ["preparing", "on the way", "delivered"];
   return (
     <div>
       <div>
